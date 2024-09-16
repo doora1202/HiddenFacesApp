07899d186001a13ad6d377635371e4b974506d31
doora <dorasyu1202@gmail.com>
2024-09-16 20:43:26 +0900

    update

--- Hidden_Faces_App.py
+++ Hidden_Faces_App.py
@@ -60,42 +60,45 @@ if back_img is not None:
         if response.ok:
             response_data = response.json()
             faces = response_data.get("faces", [])
-            for face in faces:
-                box = (np.array(face[:4], dtype=float) / scale_factor).astype(int)
-                box_w, box_h = box[2], box[3]
-                fore_size = max(box_w, box_h)
-                fore_img_resized = cv2.resize(fore_img, (fore_size, fore_size))
-                alpha_f = fore_img_resized[:, :, 3] / 255.0
-                alpha_b = 1.0 - alpha_f
+            if len(faces) == 0:
+                st.error("顔が検出されませんでした。")
+            else:
+                for face in faces:
+                    box = (np.array(face[:4], dtype=float) / scale_factor).astype(int)
+                    box_w, box_h = box[2], box[3]
+                    fore_size = max(box_w, box_h)
+                    fore_img_resized = cv2.resize(fore_img, (fore_size, fore_size))
+                    alpha_f = fore_img_resized[:, :, 3] / 255.0
+                    alpha_b = 1.0 - alpha_f
 
-                # 合成する領域を指定する
-                x1 = max(box[0], 0)
-                y1 = max(box[1], 0)
-                x2 = min(x1 + fore_size, img.shape[1])  # 元画像の幅を超えないように調整
-                y2 = min(y1 + fore_size, img.shape[0])  # 元画像の高さを超えないように調整
+                    # 合成する領域を指定する
+                    x1 = max(box[0], 0)
+                    y1 = max(box[1], 0)
+                    x2 = min(x1 + fore_size, img.shape[1])  # 元画像の幅を超えないように調整
+                    y2 = min(y1 + fore_size, img.shape[0])  # 元画像の高さを超えないように調整
 
-                # fore_img_resized の範囲を制限する
-                fore_x1 = 0 if x1 >= 0 else abs(x1)
-                fore_y1 = 0 if y1 >= 0 else abs(y1)
-                fore_x2 = fore_x1 + (x2 - x1)
-                fore_y2 = fore_y1 + (y2 - y1)
+                    # fore_img_resized の範囲を制限する
+                    fore_x1 = 0 if x1 >= 0 else abs(x1)
+                    fore_y1 = 0 if y1 >= 0 else abs(y1)
+                    fore_x2 = fore_x1 + (x2 - x1)
+                    fore_y2 = fore_y1 + (y2 - y1)
 
-                # fore_img_resized の合成範囲を切り取る
-                fore_img_crop = fore_img_resized[fore_y1:fore_y2, fore_x1:fore_x2]
-                alpha_f_crop = alpha_f[fore_y1:fore_y2, fore_x1:fore_x2]
-                alpha_b_crop = alpha_b[fore_y1:fore_y2, fore_x1:fore_x2]
+                    # fore_img_resized の合成範囲を切り取る
+                    fore_img_crop = fore_img_resized[fore_y1:fore_y2, fore_x1:fore_x2]
+                    alpha_f_crop = alpha_f[fore_y1:fore_y2, fore_x1:fore_x2]
+                    alpha_b_crop = alpha_b[fore_y1:fore_y2, fore_x1:fore_x2]
 
-                # 合成する
-                for c in range(0, 3):
-                    img[y1:y2, x1:x2, c] = (alpha_f_crop * fore_img_crop[:, :, c] +
-                                            alpha_b_crop * img[y1:y2, x1:x2, c])
-            # 表示用にBGRからRGBに変換
-            img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            st.image(img_rgb, use_column_width=True, caption='Processed Image')
+                    # 合成する
+                    for c in range(0, 3):
+                        img[y1:y2, x1:x2, c] = (alpha_f_crop * fore_img_crop[:, :, c] +
+                                                alpha_b_crop * img[y1:y2, x1:x2, c])
+                # 表示用にBGRからRGBに変換
+                img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+                st.image(img_rgb, use_column_width=True, caption='Processed Image')
 
-            # ダウンロード用にBGR形式のままエンコード
-            _, buffer = cv2.imencode('.jpg', img)  # BGRのまま
-            buffer_bytes = buffer.tobytes()
-            st.download_button(label="Download", data=buffer_bytes, file_name='processed_image.jpg', mime='image/jpg')
+                # ダウンロード用にBGR形式のままエンコード
+                _, buffer = cv2.imencode('.jpg', img)  # BGRのまま
+                buffer_bytes = buffer.tobytes()
+                st.download_button(label="Download", data=buffer_bytes, file_name='processed_image.jpg', mime='image/jpg')
         else:
-            st.error("顔が検出されませんでした。")
+            st.error("エラーが発生しました。")
